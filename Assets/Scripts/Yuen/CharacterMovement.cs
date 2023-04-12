9f6adc61f3620edc517a387c31a74add5bb51b65
Sora(HinataSato) <hinata.act.2@gmail.com>
2023-04-13 05:24:11 +0900

    移動の改善

--- Assets/Scripts/Yuen/CharacterMovement.cs
+++ Assets/Scripts/Yuen/CharacterMovement.cs
@@ -58,14 +58,6 @@ public class CharacterMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext callback)
     {
         movementinput = callback.ReadValue<Vector2>();
-        if (MathF.Abs(movementinput.x) < 1f && MathF.Abs(movementinput.x) > 0)
-        {
-            movementinput.x = 1f * MathF.Sign(movementinput.x);
-        }
-        if (MathF.Abs(movementinput.y) < 1f && MathF.Abs(movementinput.y) > 0)
-        {
-            movementinput.y = 1f * Mathf.Sign(movementinput.y);
-        }
     }
 
     //風船の追加
