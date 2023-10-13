2f4daa1569add9d43238f65c64667d948a727959
Yuen-000 <tigachan2001@gmail.com>
2023-10-13 13:32:19 +0900

    To Title

--- Assets/Scripts/Yuen/UI/ExitButton.cs
+++ Assets/Scripts/Yuen/UI/ExitButton.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Yuen.SceneLoad;
 
 namespace Yuen.UI
 {
@@ -11,13 +13,18 @@ namespace Yuen.UI
         {
             // ボタンがクリックされた時の処理を記述
             Debug.Log("Exit Game");
-            QuitGame();
+            ToTitle();
         }
 
+        // ゲームを終了する
         private void QuitGame()
         {
-            // ゲームを終了する
             Application.Quit();
+        }
+        //タイトル画面に戻る
+        private void ToTitle()
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
